1f09ba4f1009a44eb821bd653336f93037e17bb1
kolarikm <kolarikm@mail.gvsu.edu>
2016-03-17 15:49:29 -0400

    Board creation now a lambda, vertical checking working

--- c4py/board.py
+++ c4py/board.py
@@ -3,8 +3,10 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.game_board  = [[0 for i in xrange(height)] for j in xrange(width)]
-
+        #self.game_board = [[0 for i in xrange(height)] for j in xrange(width)]
+        board = lambda w, h: [[0] * w for i in range(h)]
+        self.game_board = board(width, height)
+        
     def prb(self):
         for i in self.game_board:
             for j in i:
--- c4py/engine.py
+++ c4py/engine.py
@@ -9,21 +9,6 @@ def place_token(player, column, board):
         if board[i][column] == 0:
             board[i][column] = player
             return
-'''
-def check_horizontal(board, length):
-    for r in range(0, len(board), 1):
-        for c in range(len(board[0])-length):
-            match = 0
-            print board[r][c]
-            if int(board[r][c]) != 0:
-                for x in range(0, length+1, 1):
-                    if board[r][c+x] == board[r][c]:
-                        match += 1
-                        print match
-                if match == length:
-                    return board[r][c]
-    return 0
-'''
 
 def check_h(board, length):
     for r in xrange(len(board)):
@@ -36,29 +21,44 @@ def check_h(board, length):
                 if match == length:
                     return board[r][c]
     return "No winner!"
+
+def check_v(board, length):
+    for r in xrange(len(board)-1, length, -1):
+        for c in xrange(len(board)):
+            match = 0
+            if board[r][c] != 0:
+                for x in xrange(length):
+                    if board[r-x][c] == board[r][c]:
+                        match += 1
+                if match == length:
+                    return board[r][c]
+    return "No winner!"
+
+def check_b_d(board, length):
+    i = 0
+    for r in xrange((length)-1):
+        for c in xrange(len(board[r])-length+1):
+            i += 1
+            print i
                 
 
-gb = Board(5,5)
+gb = Board(10,10)
 
 place_token(1, 1, gb.game_board)
 place_token(1, 2, gb.game_board)
 place_token(1, 3, gb.game_board)
 #place_token(1, 4, gb.game_board)
 
+place_token(1, 1, gb.game_board)
+place_token(1, 1, gb.game_board)
+place_token(1, 1, gb.game_board)
 
 gb.prb()
 
-win = check_h(gb.game_board, 4)
-print win
-'''
-for (i, row) in enumerate(gb.game_board):
-    for (j, value) in enumerate(row):
-        print gb.game_board[i][j]
-        if gb.game_board[i][j] != 0:
-            print "Yes"
+#win = check_h(gb.game_board, 4)
+#print win
 
+#w = check_v(gb.game_board, 4)
+#print w
 
-for i in range(len(gb.game_board[0])-1, -1, -1):
-    print i
-'''
-
+check_b_d(gb.game_board, 4)
